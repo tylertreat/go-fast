ddcd602d6205ce4b701e95ee9581a0b3257a6267
Tyler Treat <ttreat31@gmail.com>
2017-07-21 11:31:36 -0500

    Add trie graph

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func BenchmarkMultithreaded16Thread5050Trie(b *testing.B) {
-	numItems := 1000
+	numItems := 10000
 	numThreads := 16
 	benchmark5050(b, numItems, numThreads, func(items [][]string) matching.Matcher {
 		return matching.NewTrieMatcher()
@@ -18,25 +18,9 @@ func BenchmarkMultithreaded16Thread5050Trie(b *testing.B) {
 }
 
 func BenchmarkMultithreaded16Thread5050CSTrie(b *testing.B) {
-	numItems := 1000
+	numItems := 10000
 	numThreads := 16
 	benchmark5050(b, numItems, numThreads, func(items [][]string) matching.Matcher {
-		return matching.NewCSTrieMatcher()
-	})
-}
-
-func BenchmarkMultithreaded16Thread9010Trie(b *testing.B) {
-	numItems := 1000
-	numThreads := 16
-	benchmark9010(b, numItems, numThreads, func(items [][]string) matching.Matcher {
-		return matching.NewTrieMatcher()
-	})
-}
-
-func BenchmarkMultithreaded16Thread9010CSTrie(b *testing.B) {
-	numItems := 1000
-	numThreads := 16
-	benchmark9010(b, numItems, numThreads, func(items [][]string) matching.Matcher {
 		return matching.NewCSTrieMatcher()
 	})
 }
@@ -75,43 +59,6 @@ func benchmark5050(b *testing.B, numItems, numThreads int, factory func([][]stri
 		for j := 0; j < numThreads; j++ {
 			go func(j int) {
 				if j%2 != 0 {
-					for _, key := range itemsToInsert[j] {
-						m.Subscribe(key, sub)
-					}
-				} else {
-					for _, key := range itemsToInsert[j] {
-						m.Lookup(key)
-					}
-				}
-				wg.Done()
-			}(j)
-		}
-		wg.Wait()
-	}
-}
-
-func benchmark9010(b *testing.B, numItems, numThreads int, factory func([][]string) matching.Matcher) {
-	itemsToInsert := make([][]string, 0, numThreads)
-	for i := 0; i < numThreads; i++ {
-		items := make([]string, 0, numItems)
-		for j := 0; j < numItems; j++ {
-			topic := strconv.Itoa(j%10) + "." + strconv.Itoa(j%50) + "." + strconv.Itoa(j)
-			items = append(items, topic)
-		}
-		itemsToInsert = append(itemsToInsert, items)
-	}
-
-	var wg sync.WaitGroup
-	sub := matching.Subscriber("abc")
-	m := factory(itemsToInsert)
-	populateMatcher(m, 1000, 5)
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		wg.Add(numThreads)
-		for j := 0; j < numThreads; j++ {
-			go func(j int) {
-				if j%10 == 0 {
 					for _, key := range itemsToInsert[j] {
 						m.Subscribe(key, sub)
 					}
