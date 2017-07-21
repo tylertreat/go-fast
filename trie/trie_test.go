c30928fe3155f0a6ab062e7e66f44421bcc916f2
Tyler Treat <ttreat31@gmail.com>
2017-07-21 10:50:50 -0500

    Add trie 9-1 benchmark

--- trie/trie_test.go
+++ trie/trie_test.go
@@ -25,6 +25,22 @@ func BenchmarkMultithreaded16Thread5050CSTrie(b *testing.B) {
 	})
 }
 
+func BenchmarkMultithreaded16Thread9010Trie(b *testing.B) {
+	numItems := 1000
+	numThreads := 16
+	benchmark9010(b, numItems, numThreads, func(items [][]string) matching.Matcher {
+		return matching.NewTrieMatcher()
+	})
+}
+
+func BenchmarkMultithreaded16Thread9010CSTrie(b *testing.B) {
+	numItems := 1000
+	numThreads := 16
+	benchmark9010(b, numItems, numThreads, func(items [][]string) matching.Matcher {
+		return matching.NewCSTrieMatcher()
+	})
+}
+
 func populateMatcher(m matching.Matcher, num, topicSize int) {
 	for i := 0; i < num; i++ {
 		prefix := ""
@@ -59,6 +75,43 @@ func benchmark5050(b *testing.B, numItems, numThreads int, factory func([][]stri
 		for j := 0; j < numThreads; j++ {
 			go func(j int) {
 				if j%2 != 0 {
+					for _, key := range itemsToInsert[j] {
+						m.Subscribe(key, sub)
+					}
+				} else {
+					for _, key := range itemsToInsert[j] {
+						m.Lookup(key)
+					}
+				}
+				wg.Done()
+			}(j)
+		}
+		wg.Wait()
+	}
+}
+
+func benchmark9010(b *testing.B, numItems, numThreads int, factory func([][]string) matching.Matcher) {
+	itemsToInsert := make([][]string, 0, numThreads)
+	for i := 0; i < numThreads; i++ {
+		items := make([]string, 0, numItems)
+		for j := 0; j < numItems; j++ {
+			topic := strconv.Itoa(j%10) + "." + strconv.Itoa(j%50) + "." + strconv.Itoa(j)
+			items = append(items, topic)
+		}
+		itemsToInsert = append(itemsToInsert, items)
+	}
+
+	var wg sync.WaitGroup
+	sub := matching.Subscriber("abc")
+	m := factory(itemsToInsert)
+	populateMatcher(m, 1000, 5)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		wg.Add(numThreads)
+		for j := 0; j < numThreads; j++ {
+			go func(j int) {
+				if j%10 == 0 {
 					for _, key := range itemsToInsert[j] {
 						m.Subscribe(key, sub)
 					}
